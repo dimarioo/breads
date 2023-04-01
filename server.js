2ab59c2bdcc5d633617900f02ca9db10cce848f9
Dimario Oliver <Dimario Oliver>
2023-04-01 11:14:41 -0400

    Added Edit and Delete and Validation

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const methodOverride = require('method-override')
+const mongoose = require('mongoose')
 
 
 require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) })
+  
+  
 
 // MIDDLEWARE
 app.use(express.static('public'))
--- views/show.jsx
+++ views/show.jsx
@@ -8,7 +8,7 @@ function Show ({bread,index}) {
 <Default>
   <h3>{bread.name}</h3>
 
-<form action={`/breads/${index}?_method=DELETE`} method="POST">
+<form action={`/breads/${bread.id}?_method=DELETE`} method="POST">
   <input type='submit' value="DELETE"/>
 </form>
 
@@ -21,9 +21,10 @@ function Show ({bread,index}) {
     }
     have gluten.
   </p>
-  <img src={bread.image} alt={bread.name} />
-  <li><a href="/breads">Go home</a></li>
-  <a href={`/breads/${index}/edit`}><button>Edit</button></a>
+  <img src={bread.image} alt={bread.name}/>
+<p>Baked by {bread.baker}</p>
+<a href={`/breads/${bread.id}/edit`}><button>Edit</button></a>
+
 
 </Default>
 
