080ee6ae069f880230fcfc30bf7856b77589bfe8
Dimario Oliver <Dimario Oliver>
2023-04-10 21:29:04 -0400

    Added dotenv

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => { console.log('connected to mongo: ', process.env.MONGO_URI) })
-  
+
   
 
 // MIDDLEWARE
@@ -24,10 +24,15 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
-
-app.get('/',(req,res) => {
-    res.send('Welcome to an Awesome App about Breads')
-})
+app.get('/', async (req, res) => {
+    const foundBakers = await Baker.find().lean() 
+    const foundBreads = await Bread.find().limit(140).lean() 
+    res.render('index', {
+      breads: foundBreads,
+      bakers: foundBakers,
+      title: 'Index Page'
+    })
+  })
 
 // breads
 const breadsController = require('./controllers/breads_controller.js')
